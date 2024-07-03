0936a7473cd449bba35cffebacd705244a180007
Aiman Fariz <99475959+AimanFariz@users.noreply.github.com>
2024-07-04 01:13:31 +0800

    updated Login.js

--- screens/Login.js
+++ screens/Login.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react'
-import { View,Text,TextInput, Image, StyleSheet} from 'react-native';
+import { View,Text,TextInput, Image, StyleSheet, Pressable} from 'react-native';
+import Checkbox from 'expo-checkbox';
 
 function Login() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [isChecked, setChecked] = useState(false);
   return (
     <View style={styles.container}>
       <Image style={styles.logo} source={require('../assets/Tulsa Logos/New_Logo_2022/No BG/Tulsa_Flags_BluBG-removebg-preview.png')}/>
@@ -12,11 +14,23 @@ function Login() {
         <Text style={styles.title}>Sign In</Text>
         <TextInput style={styles.input} placeholder='Username or Email' placeholderTextColor={'#667085'}/>
         <TextInput style={styles.input} placeholder='Password' placeholderTextColor={'#667085'}/>
-        {/* Insert checkbox using react native community component */}
+        
+        {/* checkbox Remember Me */}
+        <View style={styles.checkboxContainer}>
+        <Checkbox
+          value={isChecked}
+          onValueChange={setChecked}
+          color={isChecked ? '#D0B787' : undefined}
+        />
+        <Text style={{color:'white'}}>Remember Me</Text>
+        </View>
+        {/* Sign In Button */}
+      <Pressable style={styles.button}>
+        <Text>Sign In</Text>
+      </Pressable>
       </View>
-      
-
-
+      {/* Forgot password */}
+      <Text style={{color:'white',paddingTop:20}}>Forgot password?</Text>
     </View>
   )
 }
@@ -30,6 +44,10 @@ const styles = StyleSheet.create({
     backgroundColor:'#003595',
     justifyContent:'center',
     alignItems:'center'
+  },
+  checkboxContainer:{
+    flexDirection:'row',
+    gap:10
   },
   logo:{
     width:100,
@@ -51,5 +69,17 @@ const styles = StyleSheet.create({
   signin:{
     alignItems:'flex-start',
     gap:20
+  },
+  checkbox: {
+    margin: 8,
+  },
+  button:{
+    height: 40,
+    borderWidth: 1,
+    padding: 10,
+    width:343,
+    justifyContent:'center',
+    alignItems:'center',
+    backgroundColor:'#787878',
   }
 })
